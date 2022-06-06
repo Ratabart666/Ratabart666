01c689df38f26f568017f08bc5c3d45de61a2f51
Ratabart666 <t.gomezs2@uniandes.edu.co>
2022-06-06 03:46:48 -0500

    Super update

---
+++ aplicaciones basicas/script for calification and example/aplication.bat
@@ -0,0 +1,2 @@
+pip install nbterm
+python califications.py
--- programación científica/teoría de numeros/init.py
+++ programación científica/teoría de numeros/init.py
@@ -12,11 +12,30 @@ def algoritmo_euclides(x:int,y:int)->int:
     #El proceso iterativo es encontrar un entero e y un natural r, tal que 0=<r<e
     #Estos enteros deben satisfacer que a=b*e+r
     r=a%b#residuo
-    print(str(a)+str('=')+str(b)+str('*')+str(a//b)+str('+')+str(r))
     if r!=0:
         #Si el residuo no es cero se continua el proceso iterativo
         return algoritmo_euclides(b,r)
     else:
         #cuando el residuo es cero se retorna b, que sería el MCD de (x,y)
         return b
-
+    
+    
+def menu():
+    print(100*'#')
+    print('Ingrese una opción: ')
+    print('')
+    print('1) Algoritmo de euclides')
+    print('#) Salir')
+    x=input('Ingrese la opción deseada: ')
+    if x=='1':
+        print(100*'-')
+        m=int(input('Primer número para el algoritmo de euclides: '))
+        n=int(input('Segundo número para el algoritmo de euclides: '))
+        print(algoritmo_euclides(m,n))
+        menu()
+    if x=='#':
+        print(100*'-')
+        print('Gracias por usar la aplicación')
+    else:
+        menu()
+menu()
